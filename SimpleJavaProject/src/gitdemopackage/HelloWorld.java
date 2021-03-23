526ab371e9daa8d5978a6306a8a87f9a06180bb7
Jinal Waykar <Jinal Waykar@DESKTOP-BLP5JT3>
2021-03-23 10:33:29 +0530

    Cube function added to master

--- SimpleJavaProject/src/gitdemopackage/HelloWorld.java
+++ SimpleJavaProject/src/gitdemopackage/HelloWorld.java
@@ -22,6 +22,9 @@ public static void mod(int num1,int num2){
 }
 public static void sqr(int num1,int num2){
 	System.out.println(num1*num1);
+}
+public static void cube(int num1,int num2){
+	System.out.println(num1*num1*num1);
 
 }
 	
